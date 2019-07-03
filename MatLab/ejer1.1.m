ed14f554d38be5bf2babe0f350f42d1101719836
YonatanRA <yeho@mail.com>
2019-07-03 13:40:56 +0200

    ejercicio1

---
+++ MatLab/ejer1.1.m
@@ -0,0 +1,13 @@
+U=unifrnd(0,1,2000,1);
+for i=1:2000,
+    if(U(i)>0)&&(U(i)<0.5),
+        Z(i)=U(i);
+    elseif U(i)==0.5,
+        Z(i)=0.5,
+    elseif(U(i)>0.5)&&(U(i)<1),
+        Z(i)=U(i)+0.5;
+    end
+end
+x=-0.1:0.1:1.6
+plot(x,ecdf(x,Z))
+
---
+++ MatLab/ejercicio 1.m
@@ -0,0 +1,17 @@
+%x=unifrnd(0,1,1,2000) vector aleatorio
+
+Function:F(x)
+   if x<1
+     F(x)=0
+   if 1<x<3
+    F(x)=1/8
+   if x>3
+    F(x)=1
+
+
+Function:Fy(x)
+   if x<=1/8
+    Fy(x)=1
+   if 1/8<x<1
+    Fy(x)=3
+end  
